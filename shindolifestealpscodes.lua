288501a74fc5f1b3229e756a39132168d6d1aff3
Rexurection <70076686+RexurectionV3rm@users.noreply.github.com>
2020-12-09 12:11:27 +0100

    Copies Private Server codes to clipboard

---
+++ shindolifestealpscodes.lua
@@ -0,0 +1,18 @@
+note = 'PLEASE READ: the codes will copy to clipboard every 3 secs, u should paste them into a notepad"
+warn(note)
+
+for i,v in pairs(game:GetService("Players"):GetPlayers()) do
+print(v.statz.ps.mycode.Value)
+wait(3)
+setclipboard(v.statz.ps.mycode.Value)
+end
+[[--
+--1RiHA6
+--JZSfHA
+--n50ZW-
+--tR0XCo
+--0ytUzx
+--b9_uuY
+--1RiHA6
+-- example codes i copied to clipboard
+--]]
